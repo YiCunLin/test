0e9a8df5671e02a34a61e6b8a8f4ea1b9a285571
Yee <10515120@gm.fgu.edu.tw>
2018-01-12 12:06:55 +0800

    18112

--- test/index.js
+++ test/index.js
@@ -1,11 +1,14 @@
 'use strict';
+
 window.addEventListener('load', () => {
-    console.log("index.js loaded");
+  console.log("index.js loaded");
 
-    let h1 = document.createElement('h1');
-    let msg = document.createTextNode(' 這是 <h1> 的⽂字訊息');
+  let h1 = document.createElement('h1');
+  let msg = document.createTextNode('這是 <h1> 的文字訊息');
 
-    h1.appendChild(msg);
+  h1.appendChild(msg);
 
-    document.body.appendChild(h1);
- });
+  document.body.appendChild(h1);
+});
+
+// index.js
